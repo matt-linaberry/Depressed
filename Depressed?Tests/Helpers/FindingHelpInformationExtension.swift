dd617951038488bb7ac124bfe19061290663c05e
Mark Price <mark@devslopes.com>
2016-03-06 18:51:13 -0700

    removed all tests

--- Depressed?Tests/Helpers/FindingHelpInformationExtension.swift
+++
@@ -1,9 +0,0 @@
-import Depressed
-import Foundation
-
-internal extension FindingHelpInformation {
-    init(url: NSURL, organizationName: String) {
-        self.url = url
-        self.organizationName = organizationName
-    }
-}
